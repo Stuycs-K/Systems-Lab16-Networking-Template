4ed55277f6b5dd8ea781b3917004a6c8214d7315
konstantinnovation <konstans@stuy.edu>
2023-12-22 13:21:33 -0500

    Update client.c

--- client.c
+++ client.c
@@ -6,7 +6,9 @@ void clientLogic(int server_socket){
 
 int main(int argc, char *argv[] ) {
   char* IP = "127.0.0.1";
-  if(argc>1)IP=argv[1];
+  if(argc>1){
+    IP=argv[1];
+  }
   int server_socket = client_tcp_handshake(IP);
   printf("client connected.\n");
   clientLogic(server_socket)
