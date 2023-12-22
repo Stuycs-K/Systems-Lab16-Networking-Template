8e0e08375f28848aeda36f70122c58b38a5104a9
konstantinnovation <konstans@stuy.edu>
2023-12-22 13:22:44 -0500

    Update server.c

--- server.c
+++ server.c
@@ -1,9 +1,13 @@
 #include "networking.h"
+
+void subserver_logic(int client_socket){
+
+}
+
 int main(int argc, char *argv[] ) { 
-	int client_socket = server_setup(); 
-  
+  int client_socket = server_setup(); 
+
+
   client_socket = server_tcp_handshake(client_socket);
-	
-	
 
 }
