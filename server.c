6be674323d73a09048ed1e27f6c7eda4abeb945b
konstantinnovation <konstans@stuy.edu>
2023-12-22 13:58:38 -0500

    Update server.c

--- server.c
+++ server.c
@@ -5,9 +5,9 @@ void subserver_logic(int client_socket){
 }
 
 int main(int argc, char *argv[] ) { 
-  int client_socket = server_setup(); 
+  int listen_socket = server_setup(); 
 
 
-  client_socket = server_tcp_handshake(client_socket);
+  int client_socket = server_tcp_handshake(listen_socket);
 
 }
